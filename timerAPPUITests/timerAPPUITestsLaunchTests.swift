8e9af6c6b32ef756bd5b45d34f59b8cc17ccb39d
berkenozbek <berkenozbek.swift@gmail.com>
2023-03-02 16:08:51 +0300

    Initial Commit

---
+++ timerAPPUITests/timerAPPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  timerAPPUITestsLaunchTests.swift
+//  timerAPPUITests
+//
+//  Created by Berken Özbek on 2.03.2023.
+//
+
+import XCTest
+
+final class timerAPPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
