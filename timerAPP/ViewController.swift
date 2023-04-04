493504ac2c3e8fb15e304ba399ca94678ba59bc0
Berken Özbek <124060469+berken0zbek@users.noreply.github.com>
2023-04-04 18:20:53 +0300

    Read me
    Yeni bir Xcode projesi oluşturun ve "Single View App" şablonunu seçin.
    
    Storyboard'u açın ve bir "Label" (etiket) ekleyin. Bu etiket geri sayımın gösterileceği yer olacak.
    
    "Label" öğesinin özelliklerini değiştirin. Örneğin, metni "10" olarak ayarlayabilirsiniz. Ayrıca, metnin boyutunu, rengini ve konumunu değiştirebilirsiniz.
    
    Bir "Button" (düğme) ekleyin ve "Start" olarak etiketleyin. Bu düğmeye tıklandığında geri sayım başlayacak.
    
    ViewController.swift dosyasını açın ve "Label" öğesine bir referans oluşturun.

--- timerAPP/ViewController.swift
+++ timerAPP/ViewController.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    var kalanZaman = 0
+    
+    @IBOutlet weak var labelTimer: UILabel!
+    
+    @IBAction func clickButton(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(runTime), userInfo: nil, repeats: true)
+    }
+    @objc func runTime(){
+        labelTimer.text = "Timer: \(kalanZaman)"
+        kalanZaman = kalanZaman - 1
+        if kalanZaman == 0{
+            timer.invalidate()
+            kalanZaman = 15
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        kalanZaman = 15
+        labelTimer.text = "Timer: \(kalanZaman)"
+         
     }
+    
 
 
 }
